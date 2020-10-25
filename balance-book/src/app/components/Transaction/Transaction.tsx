666f71017b6c32daa714a1266b10d40755288ee4
ksyasuda <ksyasuda@umich.edu>
2020-10-25 19:56:07 -0400

    Fix some stuff

--- balance-book/src/app/components/Transaction/Transaction.tsx
+++ balance-book/src/app/components/Transaction/Transaction.tsx
@@ -1,48 +1,48 @@
 import React from "react"
 import classes from "./Transaction.module.css"
 
-interface Props {
-	balance: number
-	transactionAmount: number
-	transactionName: string
-	transactionDate: string
-	transactionType: string
+type Props = {
+    balance: number
+    transactionAmount: number
+    transactionName: string
+    transactionDate: string
+    transactionType: string
 }
 
 const transaction: React.FunctionComponent<Props> = (props: Props) => {
-	let transChar: String, style: object
-	props.transactionType === "+" ? (transChar = "+") : (transChar = "-")
-	if (transChar === "+") {
-		style = {
-			backgroundColor: "green",
-		}
-	} else {
-		style = {
-			backgroundColor: "red",
-		}
-	}
-	const {
-		transactionName,
-		transactionAmount,
-		transactionType,
-		transactionDate,
-		balance,
-	} = props
-	const afterBalance: Number =
-		transactionType === "+"
-			? balance + transactionAmount
-			: balance - transactionAmount
-	return (
-		<div className={classes.Container} style={style}>
-			<p className={classes.TName}>
-				<span className={classes.TNameText}>{transactionName}</span> on{" "}
-				{transactionDate}
-			</p>
-			<p className={classes.TAmount}>
-				${balance} {transChar} ${transactionAmount} = ${afterBalance}
-			</p>
-		</div>
-	)
+    let transChar: String, style: object
+    props.transactionType === "+" ? (transChar = "+") : (transChar = "-")
+    if (transChar === "+") {
+        style = {
+            backgroundColor: "green",
+        }
+    } else {
+        style = {
+            backgroundColor: "red",
+        }
+    }
+    const {
+        transactionName,
+        transactionAmount,
+        transactionType,
+        transactionDate,
+        balance,
+    } = props
+    const afterBalance: Number =
+        transactionType === "+"
+            ? balance + transactionAmount
+            : balance - transactionAmount
+    return (
+        <div className={classes.Container} style={style}>
+            <p className={classes.TName}>
+                <span className={classes.TNameText}>{transactionName}</span> on{" "}
+                {transactionDate}
+            </p>
+            <p className={classes.TAmount}>
+                ${balance} {transChar} ${transactionAmount} = ${afterBalance}
+            </p>
+        </div>
+    )
 }
 
 export default transaction
--- balance-book/src/app/containers/Form/Form.tsx
+++ balance-book/src/app/containers/Form/Form.tsx
@@ -2,12 +2,12 @@ import React from "react"
 import { FunctionExpression, ObjectFlags } from "typescript"
 
 type elementConfig = {
-	name: string
-	placeholder: string
-	options?: {
-		add: { value: string }
-		sub: { value: string }
-	}
+    name: string
+    placeholder: string
+    options?: {
+        add: { value: string }
+        sub: { value: string }
+    }
 }
 
 // interface Props {
@@ -20,180 +20,181 @@ type elementConfig = {
 // }
 
 interface State {
-	changed: Function
-	value: number | string
-	elementType: string
-	elementConfig?:
-		| {
-				placehlder: string
-				name: string
-		  }
-		| { name: string }
+    changed: Function
+    value: number | string
+    elementType: string
+    elementConfig?:
+    | {
+        placehlder: string
+        name: string
+    }
+    | { name: string }
 }
 
 class Form extends React.Component<State> {
-	state = {
-		changed: null,
-		value: 0,
-		elementType: null,
-		elementConfig: {
-			placeholder: "",
-			name: "",
-		},
-	}
+    state = {
+        changed: null,
+        value: 0,
+        elementType: null,
+        elementConfig: {
+            placeholder: "",
+            name: "",
+        },
+    }
 
-	handleChange = event => {
-		console.log(event.target.value)
-		this.setState({ value: event.target.value })
-	}
+    handleChange = (event: any) => {
+        // console.log(event.target.value)
+        event.preventDefault()
+        this.setState({ value: event.target.value })
+    }
 
-	componentDidMount = () => {
-		let { changed, value, elementType, elementConfig } = this.props
-		this.setState({
-			changed: changed,
-			value: value,
-			elementType: elementType,
-			elementConfig: { ...elementConfig },
-		})
-	}
+    componentDidMount = () => {
+        let { changed, value, elementType, elementConfig } = this.props
+        this.setState({
+            changed: changed,
+            value: value,
+            elementType: elementType,
+            elementConfig: { ...elementConfig },
+        })
+    }
 
-	render() {
-		switch (this.state.elementType) {
-			case "text":
-				return (
-					<input
-						id='tname'
-						type={this.state.elementType}
-						value={this.state.value}
-						placeholder={this.state.elementConfig.placeholder}
-						name={this.state.elementConfig.name}
-						onChange={this.handleChange}
-						style={{
-							marginRight: "10px",
-							marginLeft: "10px",
-						}}
-					/>
-				)
-			case "number":
-				return (
-					<input
-						id='tamount'
-						type={this.state.elementType}
-						value={this.state.value}
-						placeholder={this.state.elementConfig.placeholder}
-						name={this.state.elementConfig.name}
-						onChange={this.handleChange}
-						style={{
-							marginRight: "10px",
-							marginLeft: "10px",
-						}}
-					/>
-				)
-			case "select":
-				return (
-					<label>
-						<span
-							style={{
-								fontWeight: "bold",
-								color: "rebeccapurple",
-							}}
-						>
-							Transaction Type
+    render() {
+        switch (this.state.elementType) {
+            case "text":
+                return (
+                    <input
+                        id='tname'
+                        type={this.state.elementType}
+                        value={this.state.value}
+                        placeholder={this.state.elementConfig.placeholder}
+                        name={this.state.elementConfig.name}
+                        onChange={this.handleChange}
+                        style={{
+                            marginRight: "10px",
+                            marginLeft: "10px",
+                        }}
+                    />
+                )
+            case "number":
+                return (
+                    <input
+                        id='tamount'
+                        type={this.state.elementType}
+                        value={this.state.value}
+                        placeholder={this.state.elementConfig.placeholder}
+                        name={this.state.elementConfig.name}
+                        onChange={this.handleChange}
+                        style={{
+                            marginRight: "10px",
+                            marginLeft: "10px",
+                        }}
+                    />
+                )
+            case "select":
+                return (
+                    <label>
+                        <span
+                            style={{
+                                fontWeight: "bold",
+                                color: "rebeccapurple",
+                            }}
+                        >
+                            Transaction Type
 						</span>
-						<select
-							name={this.state.elementConfig.name}
-							id='ttype'
-							style={{
-								margin: "0px 10px",
-							}}
-						>
-							<option label='+' value='+' id='add'>
-								+
+                        <select
+                            name={this.state.elementConfig.name}
+                            id='ttype'
+                            style={{
+                                margin: "0px 10px",
+                            }}
+                        >
+                            <option label='+' value='+' id='add'>
+                                +
 							</option>
-							<option label='-' id='add' value='-'>
-								-
+                            <option label='-' id='add' value='-'>
+                                -
 							</option>
-						</select>
-					</label>
-				)
-			case "date":
-				return (
-					<label>
-						<span
-							style={{
-								fontWeight: "bold",
-								color: "rebeccapurple",
-							}}
-						>
-							Transaction Date
+                        </select>
+                    </label>
+                )
+            case "date":
+                return (
+                    <label>
+                        <span
+                            style={{
+                                fontWeight: "bold",
+                                color: "rebeccapurple",
+                            }}
+                        >
+                            Transaction Date
 						</span>
-						<input
-							id='tdate'
-							type={this.state.elementType}
-							name={this.state.elementConfig.name}
-							value={this.state.value}
-							onChange={this.handleChange}
-							style={{
-								margin: "0px 10px",
-							}}
-						/>
-					</label>
-				)
-			case "submit":
-				return <input type='submit' />
-		}
-		return null
-	}
-	// switch (elt.elementType) {
-	// 	case "text":
-	// 		const name = (
-	// 			<Form
-	// 				key={elt.elementType}
-	// 				value={this.state.transactionName}
-	// 				changed={event => this.handleChange(event)}
-	// 			/>
-	// 		)
-	// 		formarr.push(name)
-	// 		return name
-	// 		break
-	// 	case "number":
-	// 		const num = (
-	// 			<Form
-	// 				key={elt.elementType}
-	// 				value={this.state.transactionAmount}
-	// 				changed={event => this.handleChange(event)}
-	// 			/>
-	// 		)
-	// 		formarr.push(num)
-	// 		return num
-	// 		break
-	// 	case "select":
-	// 		const type = (
-	// 			<Form
-	// 				key={elt.elementType}
-	// 				value={this.state.transactionAmount}
-	// 				changed={event => this.handleChange(event)}
-	// 			/>
-	// 		)
-	// 		formarr.push(type)
-	// 		return type
-	// 		break
-	// 	case "date":
-	// 		const date = (
-	// 			<Form
-	// 				key={elt.elementType}
-	// 				value={this.state.transactionAmount}
-	// 				changed={event => this.handleChange(event)}
-	// 			/>
-	// 		)
-	// 		formarr.push(date)
-	// 		return date
-	// 		break
-	// 	default:
-	// 		throw new Error(
-	// 			"Something went wrong with the names"
-	// 		)
-	// }
+                        <input
+                            id='tdate'
+                            type={this.state.elementType}
+                            name={this.state.elementConfig.name}
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            style={{
+                                margin: "0px 10px",
+                            }}
+                        />
+                    </label>
+                )
+            case "submit":
+                return <input type='submit' />
+        }
+        return null
+    }
+    // switch (elt.elementType) {
+    // 	case "text":
+    // 		const name = (
+    // 			<Form
+    // 				key={elt.elementType}
+    // 				value={this.state.transactionName}
+    // 				changed={event => this.handleChange(event)}
+    // 			/>
+    // 		)
+    // 		formarr.push(name)
+    // 		return name
+    // 		break
+    // 	case "number":
+    // 		const num = (
+    // 			<Form
+    // 				key={elt.elementType}
+    // 				value={this.state.transactionAmount}
+    // 				changed={event => this.handleChange(event)}
+    // 			/>
+    // 		)
+    // 		formarr.push(num)
+    // 		return num
+    // 		break
+    // 	case "select":
+    // 		const type = (
+    // 			<Form
+    // 				key={elt.elementType}
+    // 				value={this.state.transactionAmount}
+    // 				changed={event => this.handleChange(event)}
+    // 			/>
+    // 		)
+    // 		formarr.push(type)
+    // 		return type
+    // 		break
+    // 	case "date":
+    // 		const date = (
+    // 			<Form
+    // 				key={elt.elementType}
+    // 				value={this.state.transactionAmount}
+    // 				changed={event => this.handleChange(event)}
+    // 			/>
+    // 		)
+    // 		formarr.push(date)
+    // 		return date
+    // 		break
+    // 	default:
+    // 		throw new Error(
+    // 			"Something went wrong with the names"
+    // 		)
+    // }
 }
 
 export default Form
